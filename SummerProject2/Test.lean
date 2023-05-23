8a723594a5d1d5acec24a55ace39479cd00a9d3a
Suvendu Kar <sk73191491@gmail.com>
2023-05-23 16:45:30 +0300

    New vector space defined

--- SummerProject2/Test.lean
+++ SummerProject2/Test.lean
@@ -4,3 +4,7 @@ import Mathlib.Analysis.NormedSpace.CompactOperator
 #check IsCompactOperator 
 
 variable (V : Type) [AddCommGroup V] [Module ℝ V]
+
+variable (W : Type) [AddCommGroup W] [Module ℝ W] 
+
+
