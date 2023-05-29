7a5bb7693122b0264a5464c98ec13d6e1a5f5cf0
Suvendu Kar <sk73191491@gmail.com>
2023-05-29 19:04:19 +0300

    Defined trace for finite dimensional inner product space

--- SummerProject2/Test.lean
+++ SummerProject2/Test.lean
@@ -1,14 +1,27 @@
 import Mathlib.Analysis.NormedSpace.CompactOperator
+import Mathlib.Analysis.InnerProductSpace.PiL2
 
 
 #check IsCompactOperator 
 
-variable (V : Type) [AddCommGroup V] [Module ℝ V]
+variable (V : Type) [NormedAddCommGroup V] [InnerProductSpace ℂ V]
 
-variable (W : Type) [AddCommGroup W] [Module ℝ W]
+variable (W : Type) [NormedAddCommGroup W] [InnerProductSpace ℂ W]
 
 #eval 2+2
 
 #eval 2*5
+
+#check tsum
+
+#check OrthonormalBasis
+
+#check LinearMap
+
+noncomputable def OrthonormalBasis.trace [Fintype I] (e : OrthonormalBasis I ℂ V) (T : V →ₗ[ℂ] V) : ℂ :=
+  ∑' i, ⟪T (e i), e i⟫_ℂ 
+
+
+
 
 
