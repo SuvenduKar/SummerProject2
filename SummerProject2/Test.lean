2bbb22a757ce2e6f0097df83500a2c0a136e0c3b
kkytola <kalle.kytola@aalto.fi>
2023-06-06 16:04:51 +0300

    Working together on proving the additivity of trace.

--- SummerProject2/Test.lean
+++ SummerProject2/Test.lean
@@ -1,21 +1,14 @@
 import Mathlib.Analysis.NormedSpace.CompactOperator
 import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Analysis.InnerProductSpace.l2Space
+import Mathlib.Tactic.LibrarySearch
 
-
-#check IsCompactOperator 
-
+-- Let V and W be inner product spaces over ℂ.
 variable {V : Type} [NormedAddCommGroup V] [InnerProductSpace ℂ V]
-
 variable {W : Type} [NormedAddCommGroup W] [InnerProductSpace ℂ W]
 
-#eval 2+2
-
-#eval 2*5
-
 #check tsum
-
 #check OrthonormalBasis
-
 #check LinearMap
 
 /-- The trace of a linear map with respect to a given `OrthonormalBasis`.
@@ -26,15 +19,68 @@ Comment by Kalle:
 noncomputable def OrthonormalBasis.trace [Fintype I] (e : OrthonormalBasis I ℂ V) (T : V →ₗ[ℂ] V) : ℂ :=
   ∑' i, ⟪e i, T (e i)⟫_ℂ 
 
+noncomputable def HilbertBasis.trace (e : HilbertBasis I ℂ V) (T : V →ₗ[ℂ] V) : ℂ :=
+  ∑' i, ⟪e i, T (e i)⟫_ℂ 
+
 lemma OrthonormalBasis.trace_eq_trace [Fintype I] [Fintype J] 
   (e : OrthonormalBasis I ℂ V) (f : OrthonormalBasis J ℂ V) (T : V →ₗ[ℂ] V) : 
     e.trace T = f.trace T := by
   sorry
 
+/-- Trace class operators are those linear maps for which the series defining the trace
+w.r.t. some Hilbert basis is summable. -/
+class TraceClass (T : V →ₗ[ℂ] V) where
+  exists_trace : ∃ (e : HilbertBasis I ℂ V), Summable (fun i ↦ ⟪e i, T (e i)⟫_ℂ)
+
+/-- For a trace class operator, the series defining the trace w.r.t. any Hilbert
+basis is summable. -/
+lemma TraceClass.summable (T : V →ₗ[ℂ] V) [TraceClass T] (e : HilbertBasis I ℂ V) : 
+    Summable (fun i ↦ ⟪e i, T (e i)⟫_ℂ) := by
+  sorry
+
+lemma TraceClass.trace_eq_trace (T : V →ₗ[ℂ] V) [TraceClass T]
+  (e : HilbertBasis I ℂ V) (f : HilbertBasis J ℂ V) : 
+    e.trace T = f.trace T := by
+  sorry
+
+lemma HilbertBasis.trace_add (e : HilbertBasis I ℂ V) (T₁ T₂ : V →ₗ[ℂ] V)
+  [TraceClass T₁] [TraceClass T₂] : 
+    e.trace (T₁ + T₂) = e.trace T₁ + e.trace T₂ := by
+  -- Math proof?
+  --   tr(T₁ + T₂)
+  -- = ∑' i, ⟪e i, (T₁ + T₂) (e i)⟫_ℂ                       
+  -- = ∑' i, ⟪e i, T₁ (e i) + T₂ (e i)⟫_ℂ
+  -- = ∑' i, (⟪e i, T₁ (e i)⟫_ℂ + ⟪e i, T₂ (e i)⟫_ℂ)
+  -- = ∑' i, ⟪e i, T₁ (e i)⟫_ℂ + ∑' i, ⟪e i, T₂ (e i)⟫_ℂ
+  -- = tr(T₁) + tr(T₂)
+  repeat rw [trace]
+  simp only [LinearMap.add_apply]
+  simp only [inner_add_right]
+  rw [tsum_add]
+  · -- library_search
+    exact TraceClass.summable T₁ e
+  · exact TraceClass.summable T₂ e
+
 lemma OrthonormalBasis.trace_add [Fintype I]  
   (e : OrthonormalBasis I ℂ V) (T₁ T₂ : V →ₗ[ℂ] V) : 
     e.trace (T₁ + T₂) = e.trace T₁ + e.trace T₂ := by
-  sorry
+  -- Math proof?
+  --   tr(T₁ + T₂)
+  -- = ∑' i, ⟪e i, (T₁ + T₂) (e i)⟫_ℂ                       
+  -- = ∑' i, ⟪e i, T₁ (e i) + T₂ (e i)⟫_ℂ
+  -- = ∑' i, (⟪e i, T₁ (e i)⟫_ℂ + ⟪e i, T₂ (e i)⟫_ℂ)
+  -- = ∑' i, ⟪e i, T₁ (e i)⟫_ℂ + ∑' i, ⟪e i, T₂ (e i)⟫_ℂ
+  -- = tr(T₁) + tr(T₂)
+  repeat rw [trace]
+  -- rw [LinearMap.add_apply] -- Doesn't work, doesn't rewrite under binders.
+  simp only [LinearMap.add_apply]
+  simp only [inner_add_right]
+  rw [tsum_add]
+  · have observe_first := @hasSum_fintype ℂ I _ _ _ (fun i ↦ ⟪e i, T₁ (e i)⟫_ℂ) 
+    --have then_it_follows := observe_first.summable 
+    --have then_it_follows := HasSum.summable observe_first
+    exact observe_first.summable
+  · sorry
 
 lemma OrthonormalBasis.trace_smul [Fintype I] 
   (e : OrthonormalBasis I ℂ V) (α : ℂ) (T : V →ₗ[ℂ] V) : 
