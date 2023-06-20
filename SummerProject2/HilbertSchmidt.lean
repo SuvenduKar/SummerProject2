835c98176e7484559aae9f0b98a0c02f6a46f75c
Suvendu Kar <sk73191491@gmail.com>
2023-06-20 16:58:02 +0300

    Trying to prove HS operators are closed under addition

--- SummerProject2/HilbertSchmidt.lean
+++ SummerProject2/HilbertSchmidt.lean
@@ -3,6 +3,8 @@ import Mathlib.Analysis.InnerProductSpace.PiL2
 import Mathlib.Analysis.InnerProductSpace.l2Space
 import Mathlib.Tactic.LibrarySearch
 import Mathlib.Tactic.GCongr
+import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Tactic.Linarith
 
 
 
@@ -28,31 +30,37 @@ class HilbertSchmidt (T : V →ₗ[ℂ] W) where
 lemma HilbertBasis.HilbertSchmidtSummable_add (e : HilbertBasis I ℂ V) (T₁ T₂ : V →ₗ[ℂ] W)
   (h₁ : e.HilbertSchmidtSummable T₁) (h₂ : e.HilbertSchmidtSummable T₂) :
     e.HilbertSchmidtSummable (T₁ + T₂) := by
-    --simp only [HilbertSchmidtSummable]
-    --unfold HilbertSchmidtSummable at *
+  --simp only [HilbertSchmidtSummable]
+  unfold HilbertSchmidtSummable at *
+  simp only [LinearMap.add_apply]
+  simp only [inner_add_left,inner_add_right]
+  --simp only [HilbertBasis.HilbertSchmidtSummable] 
+  simp only [HilbertBasis.repr_symm_single]
+  --simp only [InnerProductSpace.Core.inner_add_add_self]--to expand inner(x+y)(x+y)
   
 
-    --have := @inner_mul_inner_self_le -- This is Cauchy-Schwarz inequality.
+
+  
+
+
+
+
+
+
+
+  --have := @inner_mul_inner_self_le -- This is Cauchy-Schwarz inequality.
   sorry
+#check Summable.smul_const
+#check Summable
+
 
 lemma HilbertBasis.HilbertSchmidtSummable_smul (e : HilbertBasis I ℂ V) (c : ℂ) (T : V →ₗ[ℂ] W)
   (h : e.HilbertSchmidtSummable T) :
     e.HilbertSchmidtSummable (c • T) := by
-    --Mathematical Proof: We have T is HS and thus ∑'i,⟪T eᵢ,T eᵢ⟫ exists finitely where is e is Hilbert Basis.
-    --Now for a complex number c ,  ∑'i,⟪c • T eᵢ, c •T eᵢ⟫ = sqr(mod(c))∑'i,⟪T eᵢ,T eᵢ⟫, which is again finite.
-    --Thus c• T is an HS.
-    unfold HilbertSchmidtSummable at *
-    simp only [HilbertSchmidtSummable]
-    simp only [LinearMap.smul_apply]
-    simp only [LinearMap.smul_apply] 
-    simp only [inner_smul_left]
-    simp only [inner_smul_right]
-    --simp only [HilbertBasis.repr_symm_single]
-    --simp only [lp.summable_inner]
-    simp [ OrthogonalFamily.summable_of_lp]
-    --simp [HilbertBasis.repr_self]
-    -- HilbertBasis.summable_inner_mul_inner
-    sorry
+unfold HilbertSchmidtSummable at *
+simp only [LinearMap.smul_apply, inner_smul_left,inner_smul_right]
+
+sorry
 
 lemma HilbertBasis.HilbertSchmidtSummable' (e : HilbertBasis I ℂ V) (T : V →ₗ[ℂ] W)
   (h : e.HilbertSchmidtSummable T) (f : HilbertBasis I ℂ V) :
