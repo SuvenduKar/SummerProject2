9c4b59dcabda6acb3b7d84c04e9dd09ffdddbe25
Suvendu Kar <sk73191491@gmail.com>
2023-06-24 12:36:35 +0300

    File to write some theorem and proof in Mathematical Logic

---
+++ SummerProject2/MathematicalLogic.lean
@@ -0,0 +1,51 @@
+import Mathlib.Tactic.Basic
+import Mathlib.Tactic.LeftRight
+
+lemma and_or_distrib_left (P Q R : Prop) : P ∧  (Q ∨ R ) ↔ (P ∧ Q) ∨ (P ∧ R) := by
+  constructor
+  · intro h 
+    cases h with
+    | intro hP hQR =>
+      cases hQR with
+      | inl hQ =>
+        left
+        constructor
+        exact hP
+        exact hQ
+      | inr hR =>
+        right 
+        constructor
+        exact hP
+        exact hR
+  · intro g
+    constructor
+    cases g with
+    | inl hPQ =>
+      cases hPQ with
+      | intro hP hQ =>
+        exact hP
+    | inr hPR =>
+      cases hPR with
+      | intro hP hR =>
+        exact hP
+    cases g with
+    | inl hPQ =>
+      left
+      cases hPQ with
+      | intro hP hQ =>
+        exact hQ
+    | inr hQR =>
+      right 
+      cases hQR with
+      | intro hQ hR =>
+        exact hR
+        
+        
+
+
+
+    
+
+
+
+
